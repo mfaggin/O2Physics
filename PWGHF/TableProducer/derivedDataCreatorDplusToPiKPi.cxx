b0739ea7048285608db4ebeb816429a11ac87b07
Mattia Faggin <mfaggin@cern.ch>
2025-04-28 15:52:17 +0200

    Propagate changes to code related to D+ analyses.
    Modifications done on core code
     - PWGHF/Core/HfMlResponseDplusToPiKPi.h
     - PWGHF/TableProducer/candidateSelectorDplusToPiKPi.cxx
    
    Analysis tasks + following code where modifications are needed:
     - [ MODIFIED ] PWGHF/TableProducer/treeCreatorDplusToPiKPi.cxx
     - [ MODIFIED ] PWGHF/TableProducer/derivedDataCreatorDplusToPiKPi.cxx
     - [ MODIFIED ] PWGHF/D2H/TableProducer/dataCreatorCharmHadPiReduced.cxx (*)
    
    (*) any PID post-calibration for the additional pion track needs to be added
    
    Analysis tasks + following code where modifications are not needed (no explicit call of Lc-daughter PID):
     - [not modified] PWGHF/Tasks/taskCharmHadImpactPar.cxx
     - [not modified] PWGHF/TableProducer/candidateCreatorB0.cxx
     - [not modified] PWGHF/HFC/Tasks/taskCorrelationDplusHadrons.cxx
     - [not modified] PWGHF/HFC/TableProducer/correlatorDplusDminus.cxx
     - [not modified] PWGHF/HFC/TableProducer/correlatorDplusHadrons.cxx
     - [not modified] PWGHF/D2H/Tasks/taskDplus.cxx
     - [not modified] PWGHF/D2H/Tasks/taskFlowCharmHadrons.cxx
     - [not modified] PWGHF/D2H/Tasks/taskB0.cxx
     - [not modified] PWGHF/D2H/Tasks/taskDirectedFlowCharmHadrons.cxx

--- PWGHF/TableProducer/derivedDataCreatorDplusToPiKPi.cxx
+++ PWGHF/TableProducer/derivedDataCreatorDplusToPiKPi.cxx
@@ -79,10 +79,10 @@ struct HfDerivedDataCreatorDplusToPiKPi {
   using CollisionsWCentMult = soa::Join<aod::Collisions, aod::CentFV0As, aod::CentFT0Ms, aod::CentFT0As, aod::CentFT0Cs, aod::PVMultZeqs>;
   using CollisionsWMcCentMult = soa::Join<aod::Collisions, aod::McCollisionLabels, aod::CentFV0As, aod::CentFT0Ms, aod::CentFT0As, aod::CentFT0Cs, aod::PVMultZeqs>;
   using TracksWPid = soa::Join<aod::Tracks, aod::TracksPidPi, aod::PidTpcTofFullPi, aod::TracksPidKa, aod::PidTpcTofFullKa>;
-  using SelectedCandidates = soa::Filtered<soa::Join<aod::HfCand3Prong, aod::HfSelDplusToPiKPi>>;
-  using SelectedCandidatesMc = soa::Filtered<soa::Join<aod::HfCand3Prong, aod::HfCand3ProngMcRec, aod::HfSelDplusToPiKPi>>;
-  using SelectedCandidatesMl = soa::Filtered<soa::Join<aod::HfCand3Prong, aod::HfSelDplusToPiKPi, aod::HfMlDplusToPiKPi>>;
-  using SelectedCandidatesMcMl = soa::Filtered<soa::Join<aod::HfCand3Prong, aod::HfCand3ProngMcRec, aod::HfSelDplusToPiKPi, aod::HfMlDplusToPiKPi>>;
+  using SelectedCandidates = soa::Filtered<soa::Join<aod::HfCand3ProngWPid, aod::HfSelDplusToPiKPi>>;
+  using SelectedCandidatesMc = soa::Filtered<soa::Join<aod::HfCand3ProngWPid, aod::HfCand3ProngMcRec, aod::HfSelDplusToPiKPi>>;
+  using SelectedCandidatesMl = soa::Filtered<soa::Join<aod::HfCand3ProngWPid, aod::HfSelDplusToPiKPi, aod::HfMlDplusToPiKPi>>;
+  using SelectedCandidatesMcMl = soa::Filtered<soa::Join<aod::HfCand3ProngWPid, aod::HfCand3ProngMcRec, aod::HfSelDplusToPiKPi, aod::HfMlDplusToPiKPi>>;
   using MatchedGenCandidatesMc = soa::Filtered<soa::Join<aod::McParticles, aod::HfCand3ProngMcGen>>;
   using TypeMcCollisions = soa::Join<aod::McCollisions, aod::McCentFT0Ms>;
 
@@ -139,15 +139,15 @@ struct HfDerivedDataCreatorDplusToPiKPi {
         candidate.impactParameterNormalised0(),
         candidate.impactParameterNormalised1(),
         candidate.impactParameterNormalised2(),
-        prong0.tpcNSigmaPi(),
-        prong0.tofNSigmaPi(),
-        prong0.tpcTofNSigmaPi(),
-        prong1.tpcNSigmaKa(),
-        prong1.tofNSigmaKa(),
-        prong1.tpcTofNSigmaKa(),
-        prong2.tpcNSigmaPi(),
-        prong2.tofNSigmaPi(),
-        prong2.tpcTofNSigmaPi());
+        candidate.nSigTpcPi0(),
+        candidate.nSigTofPi0(),
+        candidate.tpcTofNSigmaPi0(),
+        candidate.nSigTpcKa1(),
+        candidate.nSigTofKa1(),
+        candidate.tpcTofNSigmaKa1(),
+        candidate.nSigTpcPi0(),
+        candidate.nSigTofPi0(),
+        candidate.tpcTofNSigmaPi0());
     }
     if (fillCandidateParE) {
       rowCandidateParE(
