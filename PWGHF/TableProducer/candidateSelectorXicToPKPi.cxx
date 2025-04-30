e581466f1547d94249a8a7d2171c93dff2927088
Mattia Faggin <mfaggin@cern.ch>
2025-04-30 14:28:34 +0200

    Fix ML for Xic+->pKpi.

--- PWGHF/TableProducer/candidateSelectorXicToPKPi.cxx
+++ PWGHF/TableProducer/candidateSelectorXicToPKPi.cxx
@@ -364,11 +364,11 @@ struct HfCandidateSelectorXicToPKPi {
         bool isSelectedMlXicToPiKP = false;
 
         if (topolXicToPKPi && pidXicToPKPi) {
-          std::vector<float> inputFeaturesXicToPKPi = hfMlResponse.getInputFeatures(candidate, trackPos1, trackNeg, trackPos2, true);
+          std::vector<float> inputFeaturesXicToPKPi = hfMlResponse.getInputFeatures(candidate, true);
           isSelectedMlXicToPKPi = hfMlResponse.isSelectedMl(inputFeaturesXicToPKPi, ptCand, outputMlXicToPKPi);
         }
         if (topolXicToPiKP && pidXicToPiKP) {
-          std::vector<float> inputFeaturesXicToPiKP = hfMlResponse.getInputFeatures(candidate, trackPos1, trackNeg, trackPos2, false);
+          std::vector<float> inputFeaturesXicToPiKP = hfMlResponse.getInputFeatures(candidate, false);
           isSelectedMlXicToPiKP = hfMlResponse.isSelectedMl(inputFeaturesXicToPiKP, ptCand, outputMlXicToPiKP);
         }
 
